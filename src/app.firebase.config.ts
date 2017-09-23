a2d37a85bde9505ae7a1f0bea13ccea7b161bbe8
QUICK APPS STUDIOS <quickappsstudios@QUICKs-iMac.local>
2017-09-23 14:24:54 +0100

    Firebase setup complete

---
+++ src/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG={
+
+    apiKey: "AIzaSyBVIMEojotHiH6FAYW0DyqHM1A6oHDvPR4",
+    authDomain: "gymapp-38dd2.firebaseapp.com",
+    databaseURL: "https://gymapp-38dd2.firebaseio.com",
+    projectId: "gymapp-38dd2",
+    storageBucket: "gymapp-38dd2.appspot.com",
+    messagingSenderId: "491311800577"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -9,6 +10,17 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//import Angularfire
+
+import{AngularFireModule} from 'angularfire2';
+
+
+import { FIREBASE_CONFIG } from './../app.firebase.config';
+
+
+
+//Import Firebase
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +41,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+  
   ]
 })
 export class AppModule {}
